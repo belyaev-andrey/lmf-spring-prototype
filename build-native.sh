97b44e34dca39ba89c756ec344dce581d6d56c81
Andrey Belyaev <belyaev.andrey@gmail.com>
2019-08-02 13:48:39 +0300

    Manual context creation

--- build-native.sh
+++ build-native.sh
@@ -1,6 +1,12 @@
+mvn clean package
+
 native-image --verbose \
   --no-server \
   --allow-incomplete-classpath \
   --no-fallback \
   --report-unsupported-elements-at-runtime \
+  --initialize-at-build-time=org.springframework.util.Assert \
+  --initialize-at-build-time=org.springframework.core.annotation.AnnotationFilter \
+  --initialize-at-build-time=org.springframework.core.annotation.PackagesAnnotationFilter \
+  --initialize-at-build-time=org.springframework.util.StringUtils \
   -jar target/lmf-spring-prototype-1.0-SNAPSHOT.jar
--- src/main/java/org/acme/spring/event/demo/DemoApplication.java
+++ src/main/java/org/acme/spring/event/demo/DemoApplication.java
@@ -1,11 +1,16 @@
 package org.acme.spring.event.demo;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigUtils;
+import org.springframework.context.support.GenericApplicationContext;
 
 public class DemoApplication {
 
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(DemoAppConfig.class);
+        GenericApplicationContext ctx = new GenericApplicationContext();
+        ctx.registerBean(HelloBean.class);
+        ctx.registerBean(DemoEventListener.class);
+        AnnotationConfigUtils.registerAnnotationConfigProcessors(ctx);
+        ctx.refresh();
         HelloBean bean = ctx.getBean(HelloBean.class);
         System.out.println(bean.sayHello());
     }
